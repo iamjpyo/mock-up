f1427afe71c0196e9ae692c20794bcd3555737b4
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro.local>
2021-11-14 23:23:36 -0500

    Twitch API

---
+++ src/components/Challenge.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+ export const Challenge = () => {
+
+    return (
+        <div>
+            <iframe
+                src="https://www.twitch.tv/videos/1197094280&autoplay=false"
+                height="400"
+                width="600"
+                allowfullscreen="true">
+            </iframe>
+        </div>
+    )
+}
